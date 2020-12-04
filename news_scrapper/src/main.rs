e227b396ae8a9a0bf4e7ba67ec3b505705c9078d
Renato <renato145@hotmail.com>
2020-12-04 19:13:40 +1030

    first scrap

--- news_scrapper/src/main.rs
+++ news_scrapper/src/main.rs
@@ -1,4 +1,27 @@
+use scraper::{Html, Selector};
+use serde::{Serialize, Deserialize};
 
-fn main() {
-    print!("testing ghapi remove asset 2  v0.0.1\n");
+#[derive(Debug, Serialize, Deserialize)]
+struct Headline {
+    title: String,
+    url: Option<String>
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let url = "https://elcomercio.pe/ultimas-noticias/";
+    let body = reqwest::get(url).await?.text().await?;
+    let document = Html::parse_document(&body);
+    let selector = Selector::parse(".primary-font").unwrap();
+
+    for element in document.select(&selector) {
+        let title = element.text().collect::<Vec<_>>().join(" ");
+        let url = element.value().attr("href").map(String::from);
+        let headline = Headline { title, url };
+        let data = serde_json::to_string(&headline)?;
+        println!("{}", data);
+        println!("----------------------------")
+    }
+
+    Ok(())
 }
