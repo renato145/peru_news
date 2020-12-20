53d100d56214340e72c9668cfd117d4340dc1651
Renato <renato145@hotmail.com>
2020-12-20 23:50:01 +1030

    left axis

--- site/src/components/LineChart.tsx
+++ site/src/components/LineChart.tsx
@@ -1,8 +1,16 @@
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
 import { ResizeObserver } from "@juggle/resize-observer";
 import useMeasure from "react-use-measure";
 import { WordData } from "../store";
-import { line, scaleLinear, ScaleTime, max, curveNatural, axisLeft } from "d3";
+import {
+  line,
+  scaleLinear,
+  ScaleTime,
+  max,
+  curveNatural,
+  axisLeft as _axisLeft,
+  select,
+} from "d3";
 import { formatDate, formatDateStr } from "../utils";
 
 interface Props {
@@ -10,31 +18,49 @@ interface Props {
   xScale: ScaleTime<number, number>;
 }
 
-const xMarging = 5;
-const yMarging = 10;
+const margins = {
+  left: 10,
+  right: 10,
+  top: 10,
+  bottom: 10,
+};
 
 export const LineChart: React.FC<Props> = ({ data, xScale }) => {
   const [ref, { width }] = useMeasure({ polyfill: ResizeObserver });
   const height = width / 3;
-  const { values, path, timeScale, yScale } = useMemo(() => {
-    // const yDomain = extent(data.map((d) => d[1]));
+  const { values, path, timeScale, yScale, axisLeft } = useMemo(() => {
     const yDomain = [0, max(data.map((d) => d[1])) ?? 0];
     const yScale = scaleLinear()
       .domain(yDomain)
-      .range([height - yMarging, yMarging]);
-    const timeScale = xScale.range([xMarging, width - xMarging]);
+      .range([height - margins.bottom, margins.top]);
+    const timeScale = xScale.range([margins.left + 3, width - margins.right]);
     const values: [number, number][] = data.map(([date, count]) => [
       timeScale(formatDateStr(date)),
       yScale(count),
     ]);
-    const path = line().curve(curveNatural)(values);
-    return { values, path, timeScale, yScale };
+    const path = line()(values);
+    const axisLeft = _axisLeft(yScale)
+      .ticks(2)
+      .tickSize(-width + margins.left - margins.right).tickSizeOuter(0);
+    return { values, path, timeScale, yScale, axisLeft };
   }, [xScale, width, height, data]);
 
+  const xAxisRef = useCallback(
+    (node) => {
+      select(node).call(axisLeft);
+    },
+    [axisLeft]
+  );
+
   return (
-    <svg ref={ref} className="w-full my-1 bg-white rounded" height={width / 3}>
+    <svg ref={ref} className="group w-full my-1 bg-white rounded" height={width / 3}>
       {path && values ? (
         <>
+          <g
+            ref={xAxisRef}
+            transform={`translate(${margins.left},0)`}
+            className="leftAxis"
+          />
           <path
             fill="none"
             className="stroke-current text-blue-700 stroke-2 transition-all"
